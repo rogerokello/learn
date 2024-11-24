e08ae6352015d0bb40d7923582e46b046c246c5a
Roger Okello <rogerokello@gmail.com>
2024-11-24 12:44:24 +0300

    Add some explanations for the why

--- go/bk1/ch2/1/one.go
+++ go/bk1/ch2/1/one.go
@@ -5,6 +5,11 @@ import "fmt"
 func main() {
 	var i int = 20
 
+	// Literals do not have types and that is why assignment of 
+	// var f float64 = 20 would work even in we are initially
+	// assigning an integer literal. In the above case we need to
+	// do the casting because we have already associated the value
+	// with a type.
 	var f float64 = float64(i)
 
 	fmt.Println("i is ", i, "and f is ", f)
