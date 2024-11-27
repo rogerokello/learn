219516c45344cdeea8ce10a4aea3d922a09097f1
Roger Okello <rogerokello@gmail.com>
2024-11-27 22:21:56 +0300

    Add table tests

---
+++ go/bk1/ch3/3/three_test.go
@@ -0,0 +1,40 @@
+package main
+
+import "testing"
+
+func TestEmployee(t *testing.T) {
+	firstName := "Roger"
+	lastName := "Okello"
+	id := 1
+
+	testCases := []struct {
+		name     string
+		firstName, lastName string
+		id int
+		option int
+		expected Employee
+	}{
+		{
+			"Option 1", firstName, lastName, id, 1,
+			Employee{firstName, lastName, 1},
+		},
+		{
+			"Option 2", firstName, lastName, id, 2,
+			Employee{firstName, lastName, 1},
+		},
+		{
+			"Option 2", firstName, lastName, id, 3,
+			Employee{firstName, lastName, 1},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+				result := NewEmployee(tc.firstName,tc.lastName,tc.id,tc.option)
+				if result != tc.expected {
+						t.Errorf("Expected %q, got %q", tc.expected, result)
+				}
+		})
+	}
+
+}
