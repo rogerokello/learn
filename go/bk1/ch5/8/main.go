2571ba722222d9d54132002505233b925d1dd8a9
Roger Okello <rogerokello@gmail.com>
2024-12-02 18:20:16 +0300

    Solution to first exercise

---
+++ go/bk1/ch5/8/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	helloPrefix := prefixer("Hello") 
+	fmt.Println(helloPrefix("Bob")) // should print Hello Bob 
+	fmt.Println(helloPrefix("Maria")) // should print Hello Maria
+}
+
+func prefixer(prefix string) func(string) string {
+	return func(postfix string) string {
+		return prefix + " " + postfix
+	}
+}
