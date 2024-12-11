3865867a3f7887a99316a1a82d1e2ac2df2c7d2b
Roger Okello <rogerokello@gmail.com>
2024-12-11 08:42:19 +0300

    Add comments for invalid type assertions

--- go/bk1/ch7/7/main.go
+++ go/bk1/ch7/7/main.go
@@ -10,4 +10,10 @@ func main() {
 	i = j
 	isMyInt := i.(MyInt)
 	fmt.Println(isMyInt)
+
+	// You cannot use type assertions even if two types share the
+	// same underlying type. A type assertion must match the type
+	// of the value stored in the interface.
+	// The code below will panic if we do not add the ok, idiom
+	// isMyInt = i.(int)
 }
