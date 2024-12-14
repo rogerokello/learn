15a97e654b89a4afbe7ac7fcd631e8a72303252f
Roger Okello <rogerokello@gmail.com>
2024-12-14 12:02:00 +0300

    Add Reduce functionality using a generic

--- go/bk1/ch8/2/main.go
+++ go/bk1/ch8/2/main.go
@@ -14,6 +14,15 @@ func Map[T1, T2 any](s []T1, f func(T1)T2) []T2 {
 	return r
 }
 
+func Reduce[T1, T2 any](s []T1, initializer T2,f func(T2,T1)T2 ) T2 {
+	r := initializer
+
+	for _,v := range s {
+		r = f(r, v)
+	}
+	return r
+}
+
 func main(){
 	items := []string {
 		"Banana",
@@ -22,8 +31,20 @@ func main(){
 		"Pawpaw",
 		"egg",
 	}
-	items = Map(items, func(s string) string {
+	items1 := Map(items, func(s string) string {
 		return strconv.Itoa(len(s))
 	})
-	fmt.Println(items)
+	fmt.Println(items1)
+	
+	// We intend to get the length of all the items
+	// so we first get all their length stored somewhere
+	itemLengths := Map(items, func(s string) int {
+		return len(s)
+	})
+	fmt.Println(itemLengths)
+
+	sums := Reduce(itemLengths, 0, func(acc int, val int) int{
+		return acc + val
+	})
+	fmt.Println(sums)
 }
